9401fda8c13edd40306d3e476781a89d80351272
Daw1 <Daw1@MEDREGAL02>
2025-02-20 15:23:44 +0000

    Version 1: Te gusto?

---
+++ src/githubets/GitHubETS.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package githubets;
+
+/**
+ *
+ * @author Asier Aragón Atanes <aragonatanes@gmail.com>
+ */
+public class GitHubETS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Te gusto? responde con un booleano.");
+    }
+    
+}
